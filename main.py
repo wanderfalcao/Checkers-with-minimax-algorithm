03262a17c89f35ac3f03a0a334963c65afab3d43
wanderfalcao <wanderfalcao@hotmail.com>
2021-11-11 21:03:21 +0044

    Tabuleiro de damas funcional,necessitando algumas mudanças nas regras do jogo

---
+++ main.py
@@ -0,0 +1,41 @@
+import pygame 
+from damas.constants import RED, SQUARE_SIZE, WIDTH, HEIGHT
+from damas.game import Game
+
+FPS = 60
+
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Damas')
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    game = Game(WINDOW)
+
+    while run:
+        clock.tick(FPS)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
+
+        game.update()
+    
+    pygame.quit()
+
+main()
