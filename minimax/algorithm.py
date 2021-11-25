3c387a44e8f875d067b3c9ca20ccb9f9553d4c1c
wanderfalcao <wanderfalcao@hotmail.com>
2021-11-25 20:45:13 +0044

    Mudança no nome da pasta referente ao jogo de damas

--- minimax/algorithm.py
+++ minimax/algorithm.py
@@ -31,7 +31,7 @@ def get_all_moves(board, color, game):
     for piece in board.get_all_pieces(color):
         valid_moves = board.get_valid_moves(piece)
         for move, skip in valid_moves.items():
-            draw_moves(game, board, piece)
+            # draw_moves(game, board, piece)
             temp_board = deepcopy(board)
             temp_piece = temp_board.get_piece(piece.row, piece.col)
             new_board = simulate_move(temp_piece, move, temp_board, game, skip)
@@ -51,4 +51,3 @@ def draw_moves(game, board, piece):
     pygame.draw.circle(game.window, GREEN, (piece.x, piece.y), 50, 5)
     game.draw_valid_moves(valid_moves.keys())
     pygame.display.update()
-    #pygame.time.delay(100)
